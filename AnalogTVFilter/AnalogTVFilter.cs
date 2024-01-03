64bd30c718aa0c8eb879bb98c465bb66c2df4c3f
warrengalyen <wgalyen@hotmail.com>
2024-01-03 05:43:02 -0800

    Update version

--- AnalogTVFilter/AnalogTVFilter.cs
+++ AnalogTVFilter/AnalogTVFilter.cs
@@ -42,7 +42,7 @@ namespace AnalogTVFilter
         public string? DisplayName => "Analog TV";
         public string? Author => "Warren Galyen";
         public string? Copyright => "2023 Mechanika Design";
-        public Version? Version => new Version("0.7");
+        public Version? Version => new Version("0.8");
         public Uri? WebsiteUri => new Uri("https://github.com/warrengalyen/pdn-analogtv");
     }
 
